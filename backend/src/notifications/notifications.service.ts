92160c26bb72ee7e89be44a081163c389046f3be
lautato15 <lautato15@gmail.com>
2026-08-16 17:42:06 -0500

    fix imports errors

--- backend/src/notifications/notifications.service.ts
+++ backend/src/notifications/notifications.service.ts
@@ -4,7 +4,7 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { CreateNotificationDto } from './dto/create-notification.dto';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { AuthenticatedUser } from 'src/auth/decorators/current-user.decorator';
 import { EmailService } from './email/email.service';
 import { SmsService } from './sms/sms.service';
