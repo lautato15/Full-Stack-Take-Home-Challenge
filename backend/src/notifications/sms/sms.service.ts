6e169cd5ac3f8454563d7875fa70d89ef1b6f463
lautato15 <lautato15@gmail.com>
2026-08-16 17:55:22 -0500

    fix imports errors 3

--- backend/src/notifications/sms/sms.service.ts
+++ backend/src/notifications/sms/sms.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { AuthenticatedUser } from '../auth/decorators/current-user.decorator';
+import { AuthenticatedUser } from '../../auth/decorators/current-user.decorator';
 import { SendNotification } from '../types/notification';
 
 @Injectable()
