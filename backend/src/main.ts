55462673793ed5f1d2a4ce8eb88af8c1dd2448d6
lautato15 <lautato15@gmail.com>
2026-08-16 18:07:15 -0500

    Add CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -18,7 +18,10 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, documentFactory);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://notifications-appfrontend.vercel.app',
+    ],
   });
 
   await app.listen(process.env.PORT ?? 3000);
